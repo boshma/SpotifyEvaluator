727ed6f13cbdbde36bca0a665ffc9c5aa4b37241
Dahlia Xie <dahlia.xie@wsu.edu>
2023-04-23 21:20:42 -0700

    add spotifyData model and rearrange code

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 const session = require('express-session');
 const passport = require('passport');
 const SpotifyStrategy = require('passport-spotify').Strategy;
-const { User } = require('./models');
+const { User, SpotifyData } = require('./models');
 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -74,6 +75,88 @@ passport.use(new SpotifyStrategy({
           profileImage: profile.photos ? JSON.stringify(profile.photos[0]) : null
         }
       });
+      // Fetch top 5 songs and top 5 artists from Spotify API
+      const spotifyApi = new SpotifyWebApi();
+      spotifyApi.setAccessToken(accessToken);
+
+      const [topArtists, topTracks] = await Promise.all([
+        spotifyApi.getMyTopArtists({ limit: 5 }),
+        spotifyApi.getMyTopTracks({ limit: 5 })
+      ]);
+
+      // Extract relevant information from API response
+      const topArtistNames = topArtists.body.items.map(artist => artist.name);
+      const topTrackNames = topTracks.body.items.map(track => ({
+        artist: track.artists[0].name,
+        track: track.name
+      }));
+
+      const now = new Date();
+      const thirtyDaysAgo = new Date(now);
+      thirtyDaysAgo.setDate(now.getDate() - 30);
+      const listeningHistory_30days = await spotifyApi.getMyRecentlyPlayedTracks({ after: Math.floor(thirtyDaysAgo.getTime() / 1000) });
+      const history_30days = listeningHistory_30days.body.items.map(item => ({
+        artist: item.track.artists[0].name,
+        track: item.track.name,
+        album: item.track.album.name,
+        albumImage: item.track.album.images[0].url // Get the URL of the first album image
+      }));
+      const topGenres = {};
+  
+      // Loop through each item in the listening history
+      for (const item of history_30days) {
+        // Retrieve artist details using artist name
+        const artistDetails = await spotifyApi.searchArtists(item.artist);
+        const artists = artistDetails.body.artists.items;
+  
+        // Loop through the artists of the item
+        for (const artist of artists) {
+          const genres = artist.genres;
+  
+          // Loop through the genres of the artist
+          for (const genre of genres) {
+            // Check if the genre is already in the topGenres object, if not, add it with a count of 1
+            if (!topGenres[genre]) {
+              topGenres[genre] = 1;
+            } else {
+              // If the genre is already in the topGenres object, increment the count by 1
+              topGenres[genre]++;
+            }
+          }
+        }
+      }
+      const genreData = Object.entries(topGenres).map(([genre, count]) => ({
+        label: genre,
+        value: count
+      }));
+  
+      // Sort the genreData array in descending order based on the value (count)
+      genreData.sort((a, b) => b.value - a.value);
+  
+      // Get the top 5 genres
+      const topGenreNames = genreData.slice(0, 5);
+      const listeningHistory = await spotifyApi.getMyRecentlyPlayedTracks({ limit: 10 });
+      const history = listeningHistory.body.items.map(item => ({
+        artist: item.track.artists[0].name,
+        track: item.track.name,
+        album: item.track.album.name,
+        albumImage: item.track.album.images[0].url // Get the URL of the first album image
+      }));
+      const [spotifyData, alreadycreated] = await SpotifyData.findOrCreate({
+        where: { userId: user.id },
+        defaults: {
+          topArtists: JSON.stringify(topArtistNames), // Serialize array to JSON string
+          topSongs: JSON.stringify(topTrackNames), // Serialize array to JSON string
+          topGenres: JSON.stringify(topGenreNames),
+          listeningHistory: JSON.stringify(history)
+        }
+      });
+      
+      if (!alreadycreated) {
+        // If an existing instance was found and updated, log a message
+        console.log(`Spotify data for user ${user.id} updated.`);
+      }
+      //await user.setSpotifyData(spotifyData);
 
       user.accessToken = accessToken;
       user.refreshToken = refreshToken;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const router = express.Router();
 require('dotenv').config();
 const SpotifyWebApi = require('spotify-web-api-node');
+const { SpotifyData } = require('../models');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -31,112 +32,35 @@ router.get('/auth/spotify',
     // function will not be called.
   }
 );
-function countGenres(artists) {
-  const genreCount = {};
-  artists.forEach(artist => {
-    artist.genres.forEach(genre => {
-      if (genreCount[genre]) {
-        genreCount[genre]++;
-      } else {
-        genreCount[genre] = 1;
-      }
-    });
-  });
-  return genreCount;
-}
 router.get('/auth/spotify/callback', passport.authenticate('spotify', { failureRedirect: '/' }), async (req, res) => {
-  const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    accessToken: req.user.accessToken, // Use the access token from req.user
-    refreshToken: req.user.refreshToken // Use the refresh token from req.user
-  });
-
   try {
-    // Now you can use the access token to make authorized requests to the Spotify API
-    const userData = await spotifyApi.getMe(); // Example request to retrieve user data
-
-    // Retrieve user's top 5 artists
-    const topArtists = await spotifyApi.getMyTopArtists({ limit: 5 });
-    const artists = topArtists.body.items;
-
-    // Retrieve user's top 5 tracks
-    const topTracks = await spotifyApi.getMyTopTracks({ limit: 5 });
-    const tracks = topTracks.body.items.map(track => {
-      const artists = track.artists.map(artist => artist.name).join(', ');
-      return { name: track.name, artists: artists };
+    // Fetch the user's Spotify data from the database
+    const spotifyData = await SpotifyData.findOne({ where: { userId: req.user.id } });
+    const spotifyApi = new SpotifyWebApi({
+      clientId: process.env.SPOTIFY_CLIENT_ID,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      accessToken: req.user.accessToken, // Use the access token from req.user
+      refreshToken: req.user.refreshToken // Use the refresh token from req.user
     });
-    // Retrieve user's listening history from the last 30 days
-    const now = new Date();
-    const thirtyDaysAgo = new Date(now);
-    thirtyDaysAgo.setDate(now.getDate() - 30);
-    const listeningHistory_30days = await spotifyApi.getMyRecentlyPlayedTracks({ after: Math.floor(thirtyDaysAgo.getTime() / 1000) });
-    const history_30days = listeningHistory_30days.body.items.map(item => ({
-      artist: item.track.artists[0].name,
-      track: item.track.name,
-      album: item.track.album.name,
-      albumImage: item.track.album.images[0].url // Get the URL of the first album image
-    }));
-    const topGenres = {};
-
-    // Loop through each item in the listening history
-    for (const item of history_30days) {
-      // Retrieve artist details using artist name
-      const artistDetails = await spotifyApi.searchArtists(item.artist);
-      const artists = artistDetails.body.artists.items;
-
-      // Loop through the artists of the item
-      for (const artist of artists) {
-        const genres = artist.genres;
-
-        // Loop through the genres of the artist
-        for (const genre of genres) {
-          // Check if the genre is already in the topGenres object, if not, add it with a count of 1
-          if (!topGenres[genre]) {
-            topGenres[genre] = 1;
-          } else {
-            // If the genre is already in the topGenres object, increment the count by 1
-            topGenres[genre]++;
-          }
-        }
-      }
-    }
+    const userData = await spotifyApi.getMe();  
+    // Parse the JSON data from the database
+    const topArtists = JSON.parse(spotifyData.topArtists);
+    const topSongs = JSON.parse(spotifyData.topSongs);
+    const topGenres = JSON.parse(spotifyData.topGenres);
+    const history = JSON.parse(spotifyData.listeningHistory);
     
-    
-
-    // Retrieve user's listening history
-    const listeningHistory = await spotifyApi.getMyRecentlyPlayedTracks({ limit: 10 });
-    const history = listeningHistory.body.items.map(item => ({
-      artist: item.track.artists[0].name,
-      track: item.track.name,
-      album: item.track.album.name,
-      albumImage: item.track.album.images[0].url // Get the URL of the first album image
-    }));
-
-    // Convert the topGenres object to an array of objects with label and value properties
-    const genreData = Object.entries(topGenres).map(([genre, count]) => ({
-      label: genre,
-      value: count
-    }));
-
-    // Sort the genreData array in descending order based on the value (count)
-    genreData.sort((a, b) => b.value - a.value);
-
-    // Get the top 5 genres
-    const top5Genres = genreData.slice(0, 5);
-
     res.render('spotify-info', {
       title: 'Spotify Evaluator',
-      userData: userData.body,
-      artists: artists,
-      tracks: tracks,
+      userData: userData.body, // Use the user object from passport.authenticate
+      artists: topArtists,
+      tracks: topSongs,
       history: history,
-      genreData: top5Genres
+      genreData: topGenres
     });
 
   } catch (err) {
-    console.error('Error retrieving data from Spotify API:', err);
-    res.status(500).send('Error retrieving data from Spotify API');
+    console.error('Error retrieving Spotify data from database:', err);
+    res.status(500).send('Error retrieving Spotify data from database');
   }
 });
 
